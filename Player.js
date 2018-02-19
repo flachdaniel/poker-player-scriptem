f71f283f81a45a5c7baaf2415e9e55c27c7fbc69
Flach Dániel <flachdaniel77@gmail.com>
2018-02-19 11:38:05 +0100

    log

--- Player.js
+++ Player.js
@@ -6,19 +6,23 @@ class Player {
   static betRequest(gameState, bet) {
     console.log(gameState);
 
-   /* var list = ["J", "D", "K", "A"];
+   var list = ["J", "D", "K", "A"];
     var card1Rank, card2Rank;
     for (var element in gameState.players){
       if (element.name === "ScriptEm") {
         card1Rank = element.hole_cards[0].rank;
         card2Rank = element.hole_cards[1].rank;
+        console.log(card1Rank);
+        console.log(card2Rank);
       }
     }
     if(list.includes(card1Rank) || list.includes(card2Rank)) {
+
       bet(1000)
     } else {
       bet(0)
-    }*/
+    }
+
     bet(1000)
   }
 
