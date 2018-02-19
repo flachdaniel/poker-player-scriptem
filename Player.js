2ecbd8053ba3a25f2e358b1ec423b9915e3c0358
chiadx <szummerm@gmail.com>
2018-02-19 10:54:00 +0100

    list

--- Player.js
+++ Player.js
@@ -4,21 +4,20 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    // var list = ["J", "D", "K", "A"];
-    // var asd;
-    // var card1Rank, card2Rank;
-    // for (var element in gameState["players"]){
-    //   if (element["name"] === "ScriptEm") {
-    //     card1Rank = element[0]["rank"];
-    //     card2Rank = element[1]["rank"];
-    //   }
-    // }
-    // if(list.contains(card1Rank) || list.contains(card2Rank)) {
-    //   bet(1000)
-    // } else {
-    //   bet(0)
-    // }
-    bet(1000)
+    var list = ["J", "D", "K", "A"];
+    var asd;
+    var card1Rank, card2Rank;
+    for (var element in gameState[0]["players"]){
+      if (element["name"] === "ScriptEm") {
+        card1Rank = element[0]["rank"];
+        card2Rank = element[1]["rank"];
+      }
+    }
+    if(list.contains(card1Rank) || list.contains(card2Rank)) {
+      bet(1000)
+    } else {
+      bet(0)
+    }
   }
 
   static showdown(gameState) {
