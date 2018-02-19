c36d5765fecd25216404bb27cd0c1f9bda56ae22
chiadx <szummerm@gmail.com>
2018-02-19 10:18:34 +0100

    allin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/', (req, res) => {
       break;
     case 'bet_request':
       Player.betRequest(JSON.parse(game_state), function(bet) {
-        res.json(1000);
+        res.json(bet);
       });
       break;
     case 'showdown':
