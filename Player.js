2c1e902894f1e19554ba3d8aa9930964b31930ac
chiadx <szummerm@gmail.com>
2018-02-19 13:31:18 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Player.js

--- Player.js
+++ Player.js
@@ -4,28 +4,33 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    console.log(gameState);
+    try {
+      console.log(gameState);
 
-    var list = ["J", "D", "K", "A"];
-    var card1Rank, card2Rank;
-    console.log("0.2 HELLO");
-    for (var element in gameState.players){
-      console.log(element);
-      console.log(gameState.players[element].name);
-      if (gameState.players[element].name == "ScriptEm") {
-        console.log("IN");
-        console.log(gameState.players[element].hole_cards);
-        card1Rank = gameState.players[element].hole_cards[0].rank;
-        card2Rank = gameState.players[element].hole_cards[1].rank;
-        console.log(card1Rank);
-        console.log(card2Rank);
+      var list = ["J", "D", "K", "A"];
+      var card1Rank, card2Rank;
+      console.log("0.2 HELLO");
+      for (var element in gameState.players) {
+        console.log(element);
+        console.log(gameState.players[element].name);
+        if (gameState.players[element].name == "ScriptEm") {
+          console.log("IN");
+          console.log(gameState.players[element].hole_cards);
+          card1Rank = gameState.players[element].hole_cards[0].rank;
+          card2Rank = gameState.players[element].hole_cards[1].rank;
+          console.log(card1Rank);
+          console.log(card2Rank);
+        }
       }
-    }
-    if(list.includes(card1Rank) || list.includes(card2Rank)) {
-      bet(1000)
+      if (list.includes(card1Rank) || list.includes(card2Rank)) {
+        bet(1000)
 
+      }
+      bet(0)
     }
-    bet(20)
+    catch (err) {
+      bet(0)
+    }
   }
 
   static showdown(gameState) {
