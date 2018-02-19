f841e62faa23b8b9f964be1d07492e56ae57c3cb
Flach Dániel <flachdaniel77@gmail.com>
2018-02-19 15:06:15 +0100

    Counter Logic

--- Player.js
+++ Player.js
@@ -5,7 +5,6 @@ class Player {
 
   static betRequest(gameState, bet) {
     try {
-      console.log("0.2 HELLO");
       var listStrict = ["9","10","J", "D", "K", "A"];
       var listAK = ["J","Q" ,"A", "K"];
       var listLazy = ["10","J", "D", "K", "A"];
@@ -20,29 +19,34 @@ class Player {
           card2Suit = gameState.players[index].hole_cards[1].suit;
         }
       }
-      // for (var index in gameState.players) {
-      //   if (gameState.players[index].status === "active") {
-      //     ActivePlayerCounter = ActivePlayerCounter + 1;
-      //   }
-      // }
+      for (var index in gameState.players) {
+        if (gameState.players[index].status === "active") {
+          ActivePlayerCounter = ActivePlayerCounter + 1;
+        }
+      }
 
       // betelési logika
-      // if (ActivePlayerCounter >= 3) {
+      if (ActivePlayerCounter >= 3) {
+          if (listStrict.includes(card1Rank) || listStrict.includes(card2Rank)) {
+              if (card2Rank === card1Rank) {
+                  bet(5000);
+              }
+              if (listAK.includes(card1Rank) && listAK.includes(card2Rank)) {
+                  bet(5000);
+              }
+          }
+      }
+
+
       for (var index in gameState.players) {
-        if (gameState.players[index].name == "return 0") {
+        if (gameState.players[index].name === "return 0") {
           if (gameState.players[index].bet > 250) {
             bet(0);
           }
         }
       }
-        if (listStrict.includes(card1Rank) || listStrict.includes(card2Rank)) {
-            if (card2Rank === card1Rank) {
-                bet(5000);
-            }
-            if (listAK.includes(card1Rank) && listAK.includes(card2Rank)) {
-                bet(5000);
-            }
-        }
+
+
         if (listLazy.includes(card1Rank) && listLazy.includes(card2Rank)) {
           bet(gameState.pot/2);
         }
