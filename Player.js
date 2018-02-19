9d37ce5f6c3f4b410d2c70a9e16be26ed0c9cf12
Flach Dániel <flachdaniel77@gmail.com>
2018-02-19 11:22:55 +0100

    git

--- Player.js
+++ Player.js
@@ -1,30 +1,28 @@
 class Player {
-  static get VERSION() {
-    return '0.1';
-  }
-
-  static betRequest(gameState, bet) {
-
-    var list = ["J", "D", "K", "A"];
-    var card1Rank, card2Rank;
-    for (var element in gameState["players"]) {
-      if (element["name"] == "ScriptEm") {
-        card1Rank = element[0]["rank"];
-        card2Rank = element[1]["rank"];
-      }
-    }
-    if(list.contains(card1Rank) || list.contains(card2Rank)) {
-      bet(1000)
-
-    } else {
-
-      bet(0)
+    static get VERSION() {
+        return '0.1';
     }
 
-  }
+    static betRequest(gameState, bet) {
+        // var list = ["J", "D", "K", "A"];
+        // var asd;
+        // var card1Rank, card2Rank;
+        // for (var element in gameState["players"]){
+        //   if (element["name"] === "ScriptEm") {
+        //     card1Rank = element["hole_cards"][0]["rank"];
+        //     card2Rank = element["hole_cards"][1]["rank"];
+        //   }
+        // }
+        // if(list.contains(card1Rank) || list.contains(card2Rank)) {
+        //   bet(1000)
+        // } else {
+        //   bet(0)
+        // }
+        bet(1000)
+    }
 
-  static showdown(gameState) {
-  }
+    static showdown(gameState) {
+    }
 }
 
 module.exports = Player;
