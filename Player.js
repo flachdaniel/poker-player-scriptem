b9e3463227004bd2cbcac576898741f7333947d0
chiadx <szummerm@gmail.com>
2018-02-19 12:00:56 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Player.js

--- Player.js
+++ Player.js
@@ -6,29 +6,29 @@ class Player {
   static betRequest(gameState, bet) {
     //console.log(gameState);
 
-    var list = ["J", "D", "K", "A"];
-    var card1Rank, card2Rank;
-    console.log("Hello");
-    for (var element in gameState.players){
-      console.log(element);
-      console.log(gameState.players[element].name);
-      if (element.name == "ScriptEm") {
-        console.log("IN");
-        console.log(gameState.players[element].hole_cards);
-        card1Rank = gameState.players[element].hole_cards[0].rank;
-        card2Rank = gameState.players[element].hole_cards[1].rank;
-        console.log(card1Rank);
-        console.log(card2Rank);
-      }
-    }
-    if(list.includes(card1Rank) || list.includes(card2Rank)) {
-
-      bet(1000)
-
-    } else {
-
-      bet(250)
-    }
+    // var list = ["J", "D", "K", "A"];
+    // var card1Rank, card2Rank;
+    // console.log("Hello");
+    // for (var element in gameState.players){
+    //   console.log(element);
+    //   console.log(gameState.players[element].name);
+    //   if (element.name == "ScriptEm") {
+    //     console.log("IN");
+    //     console.log(gameState.players[element].hole_cards);
+    //     card1Rank = gameState.players[element].hole_cards[0].rank;
+    //     card2Rank = gameState.players[element].hole_cards[1].rank;
+    //     console.log(card1Rank);
+    //     console.log(card2Rank);
+    //   }
+    // }
+    // if(list.includes(card1Rank) || list.includes(card2Rank)) {
+    //
+    //   bet(1000)
+    //
+    // } else {
+    //
+    //   bet(250)
+    // }
 
     bet(1000)
   }
