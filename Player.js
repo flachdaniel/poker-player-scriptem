071d6c571f2ef15eca909420b88679d19d2d7ac8
Flach Dániel <flachdaniel77@gmail.com>
2018-02-19 13:47:17 +0100

    Strict playmode

--- Player.js
+++ Player.js
@@ -5,33 +5,41 @@ class Player {
 
   static betRequest(gameState, bet) {
     try {
-      console.log(gameState);
-
-      var listStrict = ["9","10","J", "D", "K", "A"];
-      var listAK = ["A", "K"]
-      var card1Rank, card2Rank;
       console.log("0.2 HELLO");
-      for (var element in gameState.players) {
-        console.log(element);
-        console.log(gameState.players[element].name);
-        if (gameState.players[element].name == "ScriptEm") {
-          console.log("IN");
-          console.log(gameState.players[element].hole_cards);
-          card1Rank = gameState.players[element].hole_cards[0].rank;
-          card2Rank = gameState.players[element].hole_cards[1].rank;
-          console.log(card1Rank);
-          console.log(card2Rank);
+      var listStrict = ["9","10","J", "D", "K", "A"];
+      var listAK = ["A", "K"];
+      var listLazy = ["10","J", "D", "K", "A"];
+      var card1Rank, card2Rank;
+      var ActivePlayerCounter = 0;
+      for (var index in gameState.players) {
+        if (gameState.players[index].name == "ScriptEm") {
+          card1Rank = gameState.players[index].hole_cards[0].rank;
+          card2Rank = gameState.players[index].hole_cards[1].rank;
         }
       }
-      if (listStrict.includes(card1Rank) || listStrict.includes(card2Rank)) {
-        if (card2Rank == card1Rank) {
-          bet(1000)
-        }
-        if (listAK.includes(card1Rank) && listAK.includes(card2Rank)) {
-          bet(1000)
+      for (var index in gameState.players) {
+        if (gameState.players[index].status == "active") {
+          ActivePlayerCounter = ActivePlayerCounter + 1;
         }
       }
-      bet(0)
+
+
+      // betelési logika
+      if (ActivePlayerCounter >= 3) {
+          if (listStrict.includes(card1Rank) || listStrict.includes(card2Rank)) {
+              if (card2Rank == card1Rank) {
+                  bet(1000);
+              }
+              if (listAK.includes(card1Rank) && listAK.includes(card2Rank)) {
+                  bet(1000);
+              }
+          }
+          bet(0);
+        }
+        if (listLazy.includes(card1Rank) && listLazy.includes(card2Rank)) {
+          bet(1000);
+        }
+        bet(0);
     }
     catch (err) {
       bet(0)
