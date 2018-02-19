4bb4ef67c419142e4d8f407a30b5da9b91f63832
chiadx <szummerm@gmail.com>
2018-02-19 11:54:02 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Player.js

--- Player.js
+++ Player.js
@@ -8,15 +8,15 @@ class Player {
 
     var list = ["J", "D", "K", "A"];
     var card1Rank, card2Rank;
-    console.error("Hello");
+    console.log("Hello");
     for (var element in gameState.players){
-      console.error(element);
-      console.error(gameState.players[element].name);
+      console.log(element);
+      console.log(gameState.players[element].name);
       if (element.name === "ScriptEm") {
         card1Rank = gameState.players[element].hole_cards[0].rank;
         card2Rank = gameState.players[element].hole_cards[1].rank;
-        console.error(card1Rank);
-        console.error(card2Rank);
+        console.log(card1Rank);
+        console.log(card2Rank);
       }
     }
     if(list.includes(card1Rank) || list.includes(card2Rank)) {
